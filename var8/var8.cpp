0237e7450fe77326eca964c15ad4aad9719489de
cmlxv <malenkijbu@yandex.ru>
2024-05-06 13:34:59 +0300

    +var7

---
+++ var8/var8.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int input(string info) {
+//    int n;
+//    cout << info;
+//    cin >> n;
+//    return n;
+//}
+//
+//int main() {
+//    int x = input("Введите число: ");
+//    int n = input("Введите систему счисления (2 <= n <= 16):");
+//    if (n < 2 || n > 16) {
+//        cout << "Ошибка ввода" << endl;
+//    } else {
+//        string digits = "0123456789ABCDEF";
+//        string str = "";
+//        while (x > 0) {
+//            int c = x % n;
+//            str += digits[c];
+//            x /= n;
+//        }
+//        reverse(str.begin(), str.end());
+//        cout << str << endl;
+//    }
+//    return 0;
+//}
